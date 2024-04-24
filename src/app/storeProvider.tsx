4eb543274783a94c46b794b6d4f3edaa1a624f3a
shifachaus <chausshifa804@gmail.com>
2024-04-24 15:37:11 +0530

    create redux store per request

--- src/app/storeProvider.tsx
+++ src/app/storeProvider.tsx
@@ -1,10 +1,17 @@
 "use client";
-import { store } from "@/lib/store/store";
-import React, { ReactNode } from "react";
+import { AppStore, makeStore } from "@/lib/store/store";
+import React, { ReactNode, useRef } from "react";
 import { Provider } from "react-redux";
 
 const StoreProvider = ({ children }: { children: ReactNode }) => {
-  return <Provider store={store}>{children}</Provider>;
+  const storeRef = useRef<AppStore>();
+
+  if (!storeRef.current) {
+    // Create the store instance the first time this renders
+    storeRef.current = makeStore();
+  }
+
+  return <Provider store={storeRef.current}>{children}</Provider>;
 };
 
 export default StoreProvider;
--- src/lib/store/store.ts
+++ src/lib/store/store.ts
@@ -1,10 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from "./features/cart/cartSlice";
 
-export const store = configureStore({
-  reducer: {},
-});
+export const makeStore = () => {
+  return configureStore({
+    reducer: {
+      cart: cartReducer,
+    },
+  });
+};
 
+// Infer the type of makeStore
+export type AppStore = ReturnType<typeof makeStore>;
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<AppStore["getState"]>;
+export type AppDispatch = AppStore["dispatch"];
